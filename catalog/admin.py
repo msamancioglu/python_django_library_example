30b01af7a4945435ce4b5b3f225e46bdebbf78c6
Mustafa Samancioglu <msamancioglu@hotmail.com>
2020-06-23 21:19:35 +0300

    Initial Commit

---
+++ catalog/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from .models import Book, Author, Genre, BookInstance, Language
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_filter = ('last_name', 'first_name')
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+#admin.site.register(BookInstance)
+
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'display_language')
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
